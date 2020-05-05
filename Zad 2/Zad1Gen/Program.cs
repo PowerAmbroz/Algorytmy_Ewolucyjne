498f5355aeaa8391b8e85d923febee528e614b1c
Ambroz <n7komandore@gmail.com>
2020-05-05 18:04:02 +0200

    Zadanie 2 i Zadanie 3

---
+++ Zad 2/Zad1Gen/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //tworzymy populację czeroelementową (takie założenie w zadaniu)
+        OperGenet popul = new OperGenet(4);
+
+        //wyświetlamy ją
+        popul.Wysw();
+        //3 razy tworzymy nową populację i oglądamy wyniki tego działania
+        for (int i = 0; i < 3; i++)
+        {
+            popul.NowaPop();
+            Console.WriteLine();
+            popul.Wysw();
+        }
+        Console.ReadKey();
+    }
+}
+
+// Jest to element podstawowy  na którym będą prowadzone operacje genetyczne
+class Osobnik : IComparable
+{
+    int[] Chromosom = new int[10];
+    public static Random r = new Random(System.DateTime.Now.Millisecond);
+    static int podz = 0;
+    //tworzymy osobnika
+    public Osobnik()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Chromosom[i] = r.Next(100);
+        }
+    }
+    //mutacja wybieramy losowo miejsce i losujemy wartość tyle razy ile podano (ile)
+    public void Mutacja(int ile)
+    {
+        for (int i = 0; i < ile; i++)
+        {
+            Chromosom[r.Next(10)] = r.Next(100);
+        }
+    }
+
+    //funkcja przystosowania 
+    public int Fitness()
+    {
+        int sum = 0;
+        int zn = 1;
+        foreach (var item in Chromosom)
+        {
+            sum += zn * item;
+            zn = -zn;
+        }
+        return Math.Abs(sum);
+    }
+
+    //wyświetlenie osobnika (chrmosomy plus funkcja fitness
+    public void Wysw()
+    {
+        foreach (var item in Chromosom)
+        {
+            Console.Write("{0,2} ", item);
+        }
+        Console.WriteLine("  {0,4}", Fitness());
+    }
+    // tworzenie nowego osobnika poprzez krzyżowanie dwóch innych (punkt krzyżowania wybrany losowo)
+    public static Osobnik operator +(Osobnik o1, Osobnik o2)
+    {
+        podz = r.Next(10);
+        Osobnik pom = new Osobnik();
+        for (int i = 0; i < podz; i++) pom.Chromosom[i] = o1.Chromosom[i];
+        for (int i = podz; i < 10; i++) pom.Chromosom[i] = o2.Chromosom[i];
+        return pom;
+    }
+    // tworzenie nowego osobnika poprzez krzyżowanie dwóch innych (punkt krzyżowania ten sam co w +) tylko odwrotnie niż w +
+    public static Osobnik operator -(Osobnik o1, Osobnik o2)
+    {
+        Osobnik pom = new Osobnik();
+        for (int i = 0; i < podz; i++) pom.Chromosom[i] = o2.Chromosom[i];
+        for (int i = podz; i < 10; i++) pom.Chromosom[i] = o1.Chromosom[i];
+        return pom;
+    }
+    //metoda potrzebna do sortowania populacji względem fitness
+    public int CompareTo(object ob)
+    {
+        return (ob as Osobnik).Fitness() - Fitness();
+    }
+}
+
+
+//tworzymy listę osobników (ilość jako paranetr w konstruktorze)
+class OperGenet : List<Osobnik>
+{
+    public static Random l = new Random(System.DateTime.Now.Millisecond);
+    public double[] przedz;
+    public OperGenet(int ile)
+    {
+        for (int i = 0; i < ile; i++)
+        {
+            Add(new Osobnik());
+        }
+        Sort();
+        OblPrzedz();
+    }
+
+    //oblicz przeedział potrzebny do losowania nowych osobników (ruletka)
+    public void OblPrzedz()
+    {
+        przedz = new double[Count];
+        double W = 0;
+        for (int i = 0; i < Count; i++) W = (przedz[i] = this[i].Fitness() + W);
+        for (int i = 0; i < Count; i++) przedz[i] = przedz[i] / W;
+    }
+
+    //mając przedział możemy powiedzieć który element będzie wybrany do krzyżowania
+    public int nrEl(double w)
+    {
+        for (int i = 0; i < Count; i++)
+            if (przedz[i] > w) return i;
+        return -1;
+    }
+
+
+    // tworzenie nowej populacji (dodajemy tyle samo nowych elementów sortujemy i odrzucamy najgorsze)
+    public void NowaPop()
+    {
+        int rozm = Count;
+        double i, j;
+        do
+        {
+            i = l.NextDouble();
+            j = l.NextDouble();
+            Add(this[nrEl(i)] + this[nrEl(j)]);
+            Add(this[nrEl(i)] - this[nrEl(j)]);
+        } while (Count < 2 * rozm);
+
+        //wprowadziłem dwie mutacje jednokrotne
+        for (int k = 0; k < 2; k++)
+        {
+            this[l.Next(Count)].Mutacja(1);
+        }
+        Sort();
+        do
+        {
+            RemoveAt(Count - 1);
+        } while (Count > rozm);
+        OblPrzedz();
+    }
+
+    //wyświetlenie populacji
+    public void Wysw()
+    {
+        foreach (var item in this)
+        {
+            item.Wysw();
+        }
+    }
+}
---
+++ Zad 3/ConsoleApp1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdgadnijTekst_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StrGen pom;
+            Console.WriteLine("Wprowadź tekst: ");
+            //haslo - tekst do odgadnięcia
+            string haslo = Console.ReadLine();
+            //tworzymy obiekt klasy StrGen (generujemy losowe znaki o długości hasła).
+            StrGen zagadka = new StrGen(haslo.Length);
+            //obliczamy funkcję przystosowania dla zagadki i wyświetlamy wygenerowane dane
+            int fitnes = zagadka.Wysw(haslo);
+            // szukamy rozwiązania do momentu aż wszystkie znaki będą poprawne (fitnes = haslo.Length)
+            while (fitnes < haslo.Length)
+            {
+                //tworzymy zmutowany obiekt
+                pom = zagadka.Mutacja();
+                //jeżeli funkcja przystosowania jest lepsza, zamieniamy go z oryginałem i wyświetlamy
+                if (pom.Fitnes(haslo) > fitnes)
+                {
+                    zagadka = pom;
+                    fitnes = zagadka.Wysw(haslo);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+
+
+    class StrGen
+    {
+        // z tych znaków będzie tworzone rozwiązanie
+        static string ZbGen = " aąbcćdeęfghijklłmnoópqrsśtuvwxyzźżABCĆDEFGHIJKLMNOÓPQRSŚTUVWXYZŹŻ!.1234567890";
+        // obiekt do generowania liczb losowych
+        public static Random r = new Random(System.DateTime.Now.Millisecond);
+        // w danych będzie prechowywany tekst do obróbki
+        List<char> dane = new List<char>();
+        // konstruktor generuje losowy ciąg znaków o podanej długości
+        public StrGen(int dl)
+        {
+            for (int i = 0; i < dl; i++)
+                dane.Add(ZbGen[r.Next(ZbGen.Length)]);
+        }
+
+        // konstruktor wczytuje podany ciąg naków 
+        public StrGen(List<char> dane)
+        {
+            this.dane.InsertRange(0, dane);
+        }
+
+
+        // tworzymy nowy obiekt który różni się jednym znakiem (losowa pozycja, losowy znak)
+        public StrGen Mutacja()
+        {
+            StrGen pom = new StrGen(dane);
+            pom.dane[r.Next(dane.Count)] = ZbGen[r.Next(ZbGen.Length)];
+            return pom;
+        }
+
+        // wyświetlenie ciągu znaków i funkcji przystosowania (zwraca funkcję prystosowania - dla wygody)
+        public int Wysw(string oryginal)
+        {
+            int pom;
+            foreach (var item in dane) Console.Write(item);
+            pom = Fitnes(oryginal);
+            Console.WriteLine("  {0}", pom);
+            return pom;
+        }
+        // funkcja przystosowania podaje ile znaków pasuje do oryginału
+        public int Fitnes(string oryginal)
+        {
+            int pom = 0, i = 0;
+            foreach (var item in dane) if (item == oryginal[i++]) pom++;
+            return pom;
+        }
+    }
+}
